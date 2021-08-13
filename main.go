6416a8a9e79b8c8462e691a6ee1b31db0a3f096f
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-08-13 14:47:54 +0900

    Update main.go
    Create First Block

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	"github.com/nohj0518/nomadcoin/person"
 )
 
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
 func main() {
-	fmt.Println("3.7 Structs with Pointers!")
-	hyeonju := person.Person{} // Create new person Hyeonju
-	hyeonju.SetDetails("hyeonju", 18)
-	fmt.Println("Main 현주", hyeonju)
+	fmt.Println("4.1 Our Fist Block!")
+	genesisBlock := block{"Genesis Block", "", ""} // First Block
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+
+	fmt.Print(genesisBlock)
 }
