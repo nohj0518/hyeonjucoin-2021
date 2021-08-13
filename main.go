7bb62729de62c90cdc636bfd2e44c8c8373480d7
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-08-14 02:36:55 +0900

    Update main.go
    Refactoring

--- main.go
+++ main.go
@@ -1,48 +1,12 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/nohj0518/hyeonjucoin-2021/blockchain"
 )
 
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-
-}
-
-func (b *blockchain) listBlocks() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("PrevHash: %s\n", block.prevHash)
-	}
-}
-
 func main() {
-	fmt.Println("4.2 Our Fist Blockchain!")
-	chain := blockchain{}
-	chain.addBlock("Genesis Block")
-	chain.addBlock("Second Block")
-	chain.addBlock("Third Block")
-	chain.listBlocks()
+	fmt.Println("4.3 Singleton Pattern!")
+	chain := blockchain.GetBlockchain()
 }
