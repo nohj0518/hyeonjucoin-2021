fcc25fc2d67d01f8fc614f4c5dba270ea6d4df43
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-08-14 18:34:28 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 )
 
 func main() {
-	fmt.Println("4.3 Singleton Pattern!")
+	fmt.Println("4.5 Refactoring part Two!")
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }
