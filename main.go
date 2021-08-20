a01b55585a59f1f20c6d19a1709ab4e334e8c486
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-08-21 01:45:33 +0900

    Update main.go

--- main.go
+++ main.go
@@ -8,24 +8,32 @@ import (
 )
 const port string = "127.0.0.1:3000"// ":4000"
 
+type URL string
+
+func (u URL) MarshalText() ([]byte, error){
+	url := fmt.Sprintf("http://%s%s",port,u)
+	return []byte(url), nil
+}
           
 type URLDescription struct{
-	URL         string `json:"url"`  
+	URL         URL `json:"url"`  
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"`
 	
-}    
+}
+
+
 
 func documentation(rw http.ResponseWriter, r *http.Request){
 	data := []URLDescription{
 		{
-			URL: "/",
+			URL: URL("/"),
 			Method: "GET",
 			Description: "See Documentation",
 		},
-		{
-			URL: "/blocks",
+		{ 
+			URL: URL("/blocks"),
 			Method: "POST",
 			Description: "Add a Block",
 			Payload: "data:string",
