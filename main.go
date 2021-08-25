a685d7cb54c9131576c5af632b749ec5412ae04b
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-08-26 01:25:21 +0900

    Update

--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/nohj0518/hyeonjucoin-2021/cli"
+	"github.com/nohj0518/hyeonjucoin-2021/db"
 )
 
 func main() {
-	difficulty := 3
-	target := strings.Repeat("0",difficulty)
-	nonce := 1
-	for {
-		hash := fmt.Sprintf("%x",sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
-		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
-		if strings.HasPrefix(hash, target){
-			return 
-		}else {
-			nonce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 }
