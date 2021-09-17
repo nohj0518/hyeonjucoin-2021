e12d9adb3858461468f1f4716347aa969f1db6a2
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-09-17 21:47:25 +0900

    Update

--- utils/utils.go
+++ utils/utils.go
@@ -21,14 +21,14 @@ func ToBytes(i interface{}) []byte {
 	return aBuffer.Bytes()
 }
 
-func FromBytes(i interface{}, data[]byte){
+func FromBytes(i interface{}, data []byte) {
 	encoder := gob.NewDecoder(bytes.NewReader(data))
 	HandleErr(encoder.Decode(i))
 }
 
-func Hash( i interface{}) string {
-	s :=fmt.Sprintf("%v",i)
+func Hash(i interface{}) string {
+	s := fmt.Sprintf("%v", i)
 	hash := sha256.Sum256([]byte(s))
-	return fmt.Sprintf("%x",hash)
+	return fmt.Sprintf("%x", hash)
 
 }
